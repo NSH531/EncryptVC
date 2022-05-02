ab5474977a289ec6631f94067db960a5f2171ca0
NSH531 <NSH531@GMAIL.COM>
2022-05-02 20:53:35 +0300

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -7,4 +7,11 @@ class encrpython():
         p.write(publicKey.save_pkcs1('PEM'))
      with open('keys/privateKey.pem', 'wb') as p:
         p.write(privateKey.save_pkcs1('PEM'))
+  def loadKeys():
+    with open('keys/publicKey.pem', 'rb') as p:
+        publicKey = rsa.PublicKey.load_pkcs1(p.read())
+    with open('keys/privateKey.pem', 'rb') as p:
+        privateKey = rsa.PrivateKey.load_pkcs1(p.read())
+    return privateKey, publicKey
+
         
