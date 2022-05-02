295da5e71351cdc4211ef114e4c76415636785e3
NSH531 <NSH531@GMAIL.COM>
2022-05-02 14:42:49 +0300

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -2,20 +2,27 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import binascii
-
 keyPair = RSA.generate(8192)
+class encrpython(keyPair):
+  
+  def pubKey(keyPair):
+    
+    pubKey = keyPair.publickey()
+    print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+     pubKeyPEM = pubKey.exportKey()
 
-pubKey = keyPair.publickey()
-print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
-pubKeyPEM = pubKey.exportKey()
-print(pubKeyPEM.decode('ascii'))
-
-print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
-privKeyPEM = keyPair.exportKey()
-print(privKeyPEM.decode('ascii'))
-
-#encryption
-msg = 'A message for encryption'
-encryptor = PKCS1_OAEP.new(pubKey)
-encrypted = encryptor.encrypt(msg)
-print("Encrypted:", binascii.hexlify(encrypted))
+    print(pubKeyPEM.decode('ascii'))
+    return  pubKeyPEM
+  
+  #print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+  def privKey(KeyPair):
+    
+    privKeyPEM = keyPair.exportKey()
+    print(privKeyPEM.decode('ascii'))
+    return privKeyPEM
+  def encryption(msg,pubKey):
+    encryptor = PKCS1_OAEP.new(pubKey)
+    encrypted = encryptor.encrypt(msg)
+    print("Encrypted:", binascii.hexlify(encrypted))
+    return encrypted
+  
