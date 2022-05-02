a1133923e76ed9346b05ca2917b7ebedf76a1112
NSH531 <NSH531@GMAIL.COM>
2022-05-02 20:52:37 +0300

    Delete IO.py

--- IO.py
+++
@@ -1,7 +0,0 @@
-class IO_versions():
-  Ecrypt=encrpython()
-  def load(Ecrypt):
-    keys=[]
-    keys.append(Ecrypt.Pubkey(Ecrypt.KeyPair))
-    keys.append(Ecrypt.PrivKey(Ecrypt.KeyPair))
-    return keys
