195f887c286238ba440f31fef7c5eaf851af14b1
NSH531 <NSH531@GMAIL.COM>
2022-05-02 20:51:42 +0300

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,28 +1,10 @@
-
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+import rsa
 import binascii
 class encrpython():
-  keyPair = RSA.generate(8192)
-  
-  def pubKey(keyPair):
-    
-    pubKey = keyPair.publickey()
-    print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
-     pubKeyPEM = pubKey.exportKey()
-
-    print(pubKeyPEM.decode('ascii'))
-    return  pubKeyPEM
-  
-  #print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
-  def privKey(KeyPair):
-    
-    privKeyPEM = keyPair.exportKey()
-    print(privKeyPEM.decode('ascii'))
-    return privKeyPEM
-  def encryption(msg,pubKey):
-    encryptor = PKCS1_OAEP.new(pubKey)
-    encrypted = encryptor.encrypt(msg)
-    print("Encrypted:", binascii.hexlify(encrypted))
-    return encrypted
-  
+  def generateKeys():
+    (publicKey, privateKey) = rsa.newkeys(8192)
+    with open('keys/publcKey.pem', 'wb') as p:
+        p.write(publicKey.save_pkcs1('PEM'))
+     with open('keys/privateKey.pem', 'wb') as p:
+        p.write(privateKey.save_pkcs1('PEM'))
+        
